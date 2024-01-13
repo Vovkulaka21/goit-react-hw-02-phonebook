9d85ae50f6e557cc89b239917b23a17e6c12d1c3
Vovkulaka21 <Vovkulaka16@gmail.com>
2024-01-13 16:29:14 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,9 @@ import { Component } from 'react';
 // import { nanoid } from 'nanoid';
 
 export class App extends Component {
+
   render() {
+    
     return (
       <div className={css.box}>
         <form className={css.form}>
