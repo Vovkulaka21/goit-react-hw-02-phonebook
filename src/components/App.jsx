a253c5e169fbe9dde45eed20e1f4e9355a91f7f6
Vovkulaka21 <Vovkulaka16@gmail.com>
2024-01-13 03:22:02 +0200

    Start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,16 @@
+import css from './App.module.css'
+
 import { Component } from 'react';
+
+
+// import { nanoid } from 'nanoid';
 
 export class App extends Component {
 
   render() {
     return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-
+      <div className={css.box}>
+      
       </div>
     );
   }
