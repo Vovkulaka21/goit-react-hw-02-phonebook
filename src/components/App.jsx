5e3d092fe949c1f2276f31869afe3d599e473cc6
Vovkulaka21 <Vovkulaka16@gmail.com>
2024-01-14 02:22:25 +0200

    Refactor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,44 +2,23 @@ import css from './App.module.css';
 
 import { Component } from 'react';
 
-import { nanoid } from 'nanoid';
+// import { nanoid } from 'nanoid';
 
-export class App extends Component {
+import AddContactForm from './AddContactForm/AddContactForm';
 
-  idName = nanoid();
-  idNumber = nanoid();
+class App extends Component {
+
+  state = {
+    contacts: [],
+    filter: '',
+  };
 
   render() {
 
-    const {idName, idNumber} = this;
-
     return (
+
       <div className={css.box}>
-        <form className={css.form}>
-          <label htmlFor={idName}>Name</label>
-          <input
-            id={idName}
-            placeholder="Name"
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-          <label htmlFor={idNumber}>Number</label>
-          <input
-            id={idNumber}
-            placeholder="Number"
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-          <button type="submit" className={css.btn}>
-            Add contact
-          </button>
-        </form>
+        <AddContactForm/>
         <div className={css.contact_block}>
           <form className={css.form_find}>
             <label>Find contacts by name</label>
@@ -70,3 +49,5 @@ export class App extends Component {
     );
   }
 }
+
+export default App
