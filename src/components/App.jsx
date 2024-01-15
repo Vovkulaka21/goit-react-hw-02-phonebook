68647ca9141c3c7d2f9cc6b7ba10ae670f607b67
Vovkulaka21 <Vovkulaka16@gmail.com>
2024-01-15 19:59:35 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,11 +8,30 @@ import Phonebook from './Phonebook/Phonebook';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [ {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'}],
     filter: '',
   };
 
+  dublicateName({name}) {
+    const normalizeName = name.toLowerCase().replaceAll(' ', '');
+    const { contacts } = this.state;
+
+    const dublicate = contacts.find(item => {
+      const normalizeCurrentName = item.name.toLowerCase().replaceAll(' ', '');
+      return normalizeCurrentName === normalizeName;
+    });
+
+    return Boolean(dublicate)
+
+  }
+
   addContact = info => {
+    const {name} = info;
+
+    if (this.dublicateName(info)) {
+      return alert(`${name} is already in contacts.`);
+    }
+    
     this.setState(({ contacts }) => {
       const newContact = {
         id: nanoid(),
